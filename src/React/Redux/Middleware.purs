c8d5cc6e0bb46dc2f3d7ff2a37c30314b7291e6e
Pavlos Georgiou <pavlos.georgiou.p@gmail.com>
2018-06-24 22:01:57 +0100

    Updates for 0.12

--- src/React/Redux/Middleware.purs
+++ src/React/Redux/Middleware.purs
@@ -2,32 +2,31 @@ module React.Redux.Middleware where
 
 import Prelude
 
-import Control.Monad.Eff (Eff)
+import Effect (Effect)
 
-import Data.Monoid (class Monoid, mempty)
 import Data.Newtype (class Newtype, unwrap)
 
-import React.Redux.Types (ReduxEffect, Dispatch)
+import React.Redux.Types (Dispatch)
 
-type MiddlewareAPI eff state action
-  = { getState :: Eff (ReduxEffect eff) state
-    , dispatch :: Dispatch eff action action
+type MiddlewareAPI state action
+  = { getState :: Effect state
+    , dispatch :: Dispatch action action
     }
 
-newtype Middleware eff state action a b = Middleware (MiddlewareAPI eff state action -> Dispatch eff action a -> Dispatch eff action b)
+newtype Middleware state action a b = Middleware (MiddlewareAPI state action -> Dispatch action a -> Dispatch action b)
 
-derive instance newtypeMiddleware :: Newtype (Middleware eff state action a b) _
+derive instance newtypeMiddleware :: Newtype (Middleware state action a b) _
 
-instance semigroupoidMiddleware :: Semigroupoid (Middleware eff state action) where
+instance semigroupoidMiddleware :: Semigroupoid (Middleware state action) where
   compose (Middleware f) (Middleware g) = Middleware (\api -> f api <<< g api)
 
-instance categoryMiddleware :: Category (Middleware eff state action) where
-  id = Middleware (const id)
+instance categoryMiddleware :: Category (Middleware state action) where
+  identity = Middleware (const identity)
 
-instance functorMiddleware :: Functor (Middleware eff state action a) where
+instance functorMiddleware :: Functor (Middleware state action a) where
   map f (Middleware g) = Middleware (\api next action -> f <$> g api next action)
 
-instance applyMiddleware :: Apply (Middleware eff state action a) where
+instance applyMiddleware :: Apply (Middleware state action a) where
   apply (Middleware f) (Middleware g) =
     Middleware $ \api next action -> do
       a <- g api next action
@@ -36,19 +35,19 @@ instance applyMiddleware :: Apply (Middleware eff state action a) where
 
       pure (k a)
 
-instance applicativeMiddleware :: Applicative (Middleware eff state action a) where
+instance applicativeMiddleware :: Applicative (Middleware state action a) where
   pure a = Middleware (\_ _ _ -> pure a)
 
-instance bindMiddleware :: Bind (Middleware eff state action a) where
+instance bindMiddleware :: Bind (Middleware state action a) where
   bind (Middleware m) f =
     Middleware $ \api next action -> do
       a <- m api next action
 
       unwrap (f a) api next action
 
-instance monadMiddleware :: Monad (Middleware eff state action a)
+instance monadMiddleware :: Monad (Middleware state action a)
 
-instance semigroupMiddleware :: Semigroup b => Semigroup (Middleware eff state action a b) where
+instance semigroupMiddleware :: Semigroup b => Semigroup (Middleware state action a b) where
   append (Middleware f) (Middleware g) =
     Middleware $ \api next action -> do
       b1 <- f api next action
@@ -57,5 +56,5 @@ instance semigroupMiddleware :: Semigroup b => Semigroup (Middleware eff state a
 
       pure (b1 <> b2)
 
-instance monoidMiddleware :: Monoid b => Monoid (Middleware eff state action a b) where
+instance monoidMiddleware :: Monoid b => Monoid (Middleware state action a b) where
   mempty = Middleware (const (const (const (pure mempty))))
